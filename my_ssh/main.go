486883cf854b53412e72ca5a582a4289c9bf0f31
Jiahan Tu <jiahan.tu@Jiahans-MacBook-Pro.local>
2023-08-16 22:05:43 -0700

    draft version for ssh

---
+++ my_ssh/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"my_ssh/tools"
+)
+
+func main() {
+	tools.Hello()
+}
---
+++ my_ssh/script.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"my_ssh/tools"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/term"
+)
+
+func script() error {
+	// Create arbitrary command.
+	shell := os.Getenv("SHELL")
+	c := exec.Command(shell)
+
+	// Start the command with a pty.
+	ptmx, err := tools.Start(c)
+	if err != nil {
+		return err
+	}
+	// Make sure to close the pty at the end.
+	defer func() { _ = ptmx.Close() }() // Best effort.
+
+	// Handle pty size.
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGWINCH)
+	go func() {
+		for range ch {
+			if err := tools.InheritSize(os.Stdin, ptmx); err != nil {
+				log.Printf("error resizing pty: %s", err)
+			}
+		}
+	}()
+	ch <- syscall.SIGWINCH                        // Initial resize.
+	defer func() { signal.Stop(ch); close(ch) }() // Cleanup signals when done.
+
+	// Set stdin in raw mode.
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = term.Restore(int(os.Stdin.Fd()), oldState) }() // Best effort.
+
+	// Copy stdin to the pty and the pty to stdout.
+	// NOTE: The goroutine will keep reading until the next keystroke before returning.
+	go func() { _, _ = io.Copy(ptmx, os.Stdin) }()
+	file, err := os.Create("command_results.txt")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	defer file.Close()
+	io.Copy(io.MultiWriter(os.Stdout, file), ptmx)
+
+	return nil
+}
+
+func main() {
+	if err := script(); err != nil {
+		log.Fatal(err)
+	}
+}
